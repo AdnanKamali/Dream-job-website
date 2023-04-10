abed5e66f4718b10b17c26f0d427b8d659c45f89
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 10:29:06 +0330

    sample detail page is created

---
+++ dream_job_front/pages/browse/[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="py-10 px-6 grid md:grid-cols-4 gap-3">
+    <div class="md:col-span-3">
+      <h1 class="mb-6 text-3xl">The Title</h1>
+
+      <p>The description of the jobs...</p>
+
+      <a
+        href="a"
+        class="inline-block mt-6 py-4 px-6 bg-teal-700 text-white rounded-xl"
+        >Apply</a
+      >
+    </div>
+    <div class="md:col-span-1 py-6 px-6 bg-teal-700 text-white rounded-xl">
+      <h1 class="text-3xl mb-6">Company</h1>
+
+      <p class="mb-2">Company name</p>
+      <p>Location</p>
+
+      <hr class="opacity-30 my-6" />
+
+      <h1 class="text-3xl mb-6">Position</h1>
+
+      <p class="mb-2">Position location</p>
+      <p>Salary</p>
+      <hr class="opacity-30 my-6" />
+
+      <p>Posted at Dec. 1. 2022</p>
+    </div>
+  </div>
+</template>
