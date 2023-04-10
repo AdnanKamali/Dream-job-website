4a0bbb76d3b4964879fbf8bd015ff0d89aab7fb0
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 11:07:32 +0330

    sample create job page created

--- dream_job_front/pages/signup.vue
+++ dream_job_front/pages/signup.vue
@@ -2,26 +2,27 @@
   <div class="py-10 px-6">
     <div class="max-w-sm mx-auto py-10 px-6 bg-gray-100 rounded-xl">
       <h1 class="mb-6 text-2xl">Sign up</h1>
+      <form action="">
+        <input
+          type="email"
+          placeholder="Your email address..."
+          class="w-full mb-4 py-4 px-6 rounded-xl"
+        />
+        <input
+          type="password"
+          placeholder="Your password..."
+          class="w-full mb-4 py-4 px-6 rounded-xl"
+        />
+        <input
+          type="password"
+          placeholder="Repeat password..."
+          class="w-full mb-4 py-4 px-6 rounded-xl"
+        />
 
-      <input
-        type="email"
-        placeholder="Your email address..."
-        class="w-full mb-4 py-4 px-6 rounded-xl"
-      />
-      <input
-        type="password"
-        placeholder="Your password..."
-        class="w-full mb-4 py-4 px-6 rounded-xl"
-      />
-      <input
-        type="password"
-        placeholder="Repeat password..."
-        class="w-full mb-4 py-4 px-6 rounded-xl"
-      />
-
-      <button class="py-4 px-6 bg-teal-700 rounded-xl text-white">
-        Submit
-      </button>
+        <button class="py-4 px-6 bg-teal-700 rounded-xl text-white">
+          Submit
+        </button>
+      </form>
     </div>
   </div>
 </template>
