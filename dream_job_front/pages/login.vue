871fd15a85c8a7cc202a92943c646f4a0825a33e
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 10:36:42 +0330

    sample login page created

---
+++ dream_job_front/pages/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="py-10 px-6">
+    <div class="max-w-sm mx-auto py-10 px-6 bg-gray-100 rounded-xl">
+      <h1 class="mb-6 text-2xl">Log in</h1>
+
+      <input
+        type="email"
+        placeholder="Your email address..."
+        class="w-full mb-4 py-4 px-6 rounded-xl"
+      />
+      <input
+        type="password"
+        placeholder="Your password..."
+        class="w-full mb-4 py-4 px-6 rounded-xl"
+      />
+
+      <button class="py-4 px-6 bg-teal-700 rounded-xl text-white">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
