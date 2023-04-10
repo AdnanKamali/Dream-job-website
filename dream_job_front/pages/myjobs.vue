f5c47c834a341ee3d9daa239d0d7e1d3ec71e0d9
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 17:40:20 +0330

    sample my jobs page created

---
+++ dream_job_front/pages/myjobs.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="py-10 px-6">
+    <h1 class="mb-6 text-2xl">My Jobs</h1>
+    <div>
+      <div class="p-6 flex items-center justify-between bg-gray-100">
+        <div>
+          <h1 class="text-xl font-semibold">The job position</h1>
+          <p class="text-gray-600">The company name</p>
+        </div>
+        <div>
+          <h1 class="font-semibold">Worldwide</h1>
+          <p class="text-gray-600">$90-120$</p>
+        </div>
+        <div>
+          <p>Posted Dec. 1. 2022</p>
+        </div>
+        <div class="space-x-4">
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+            >Detail</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-cyan-700 text-white rounded-xl"
+            >Edit</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-rose-700 text-white rounded-xl"
+            >Delete</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
+    <div class="mt-4">
+      <div class="p-6 flex items-center justify-between bg-gray-100">
+        <div>
+          <h1 class="text-xl font-semibold">The job position</h1>
+          <p class="text-gray-600">The company name</p>
+        </div>
+        <div>
+          <h1 class="font-semibold">Worldwide</h1>
+          <p class="text-gray-600">$90-120$</p>
+        </div>
+        <div>
+          <p>Posted Dec. 1. 2022</p>
+        </div>
+        <div class="space-x-4">
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+            >Detail</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-cyan-700 text-white rounded-xl"
+            >Edit</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-rose-700 text-white rounded-xl"
+            >Delete</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
+    <div class="mt-4">
+      <div class="p-6 flex items-center justify-between bg-gray-100">
+        <div>
+          <h1 class="text-xl font-semibold">The job position</h1>
+          <p class="text-gray-600">The company name</p>
+        </div>
+        <div>
+          <h1 class="font-semibold">Worldwide</h1>
+          <p class="text-gray-600">$90-120$</p>
+        </div>
+        <div>
+          <p>Posted Dec. 1. 2022</p>
+        </div>
+        <div class="space-x-4">
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+            >Detail</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-cyan-700 text-white rounded-xl"
+            >Edit</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-rose-700 text-white rounded-xl"
+            >Delete</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
+    <div class="mt-4">
+      <div class="p-6 flex items-center justify-between bg-gray-100">
+        <div>
+          <h1 class="text-xl font-semibold">The job position</h1>
+          <p class="text-gray-600">The company name</p>
+        </div>
+        <div>
+          <h1 class="font-semibold">Worldwide</h1>
+          <p class="text-gray-600">$90-120$</p>
+        </div>
+        <div>
+          <p>Posted Dec. 1. 2022</p>
+        </div>
+        <div class="space-x-4">
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+            >Detail</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-cyan-700 text-white rounded-xl"
+            >Edit</nuxt-link
+          >
+          <nuxt-link
+            to="/browse/1"
+            class="py-4 px-6 bg-rose-700 text-white rounded-xl"
+            >Delete</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
