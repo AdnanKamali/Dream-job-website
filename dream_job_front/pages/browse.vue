b2bd65774f02ac32ce656a4881f3b4223bf7b46e
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 10:08:00 +0330

    sample browse page complete

---
+++ dream_job_front/pages/browse.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="grid md:grid-cols-4 gap-3 py-10 px-6">
+    <div class="md:col-span-1 px-6 py-6 bg-teal-700 rounded-xl">
+      <div class="flex space-x-4">
+        <input
+          type="search"
+          placeholder="Find a job..."
+          class="w-full px-6 py-6 rounded-xl"
+        />
+        <button class="px-6 py-4 bg-teal-900 text-white rounded-xl">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </button>
+      </div>
+      <hr class="my-6 opacity-30" />
+
+      <h3 class="mt-6 text-xl text-white">Categories</h3>
+
+      <div class="mt-6 space-y-4">
+        <p class="py-4 px-6 text-white rounded-xl">Category 1</p>
+        <p class="py-4 px-6 text-white rounded-xl">Category 2</p>
+        <p class="py-4 px-6 text-white rounded-xl">Category 3</p>
+      </div>
+    </div>
+    <div class="col-span-3">
+      <div>
+        <div class="p-6 flex items-center justify-between bg-gray-100">
+          <div>
+            <h1 class="text-xl font-semibold">The job position</h1>
+            <p class="text-gray-600">The company name</p>
+          </div>
+          <div>
+            <h1 class="font-semibold">Worldwide</h1>
+            <p class="text-gray-600">$90-120$</p>
+          </div>
+          <div>
+            <p>Posted Dec. 1. 2022</p>
+          </div>
+          <div>
+            <nuxt-link
+              to="/browse/1"
+              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+              >Detail</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <div class="p-6 flex items-center justify-between bg-gray-100">
+          <div>
+            <h1 class="text-xl font-semibold">The job position</h1>
+            <p class="text-gray-600">The company name</p>
+          </div>
+          <div>
+            <h1 class="font-semibold">Worldwide</h1>
+            <p class="text-gray-600">$90-120$</p>
+          </div>
+          <div>
+            <p>Posted Dec. 1. 2022</p>
+          </div>
+          <div>
+            <nuxt-link
+              to="/browse/1"
+              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+              >Detail</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <div class="p-6 flex items-center justify-between bg-gray-100">
+          <div>
+            <h1 class="text-xl font-semibold">The job position</h1>
+            <p class="text-gray-600">The company name</p>
+          </div>
+          <div>
+            <h1 class="font-semibold">Worldwide</h1>
+            <p class="text-gray-600">$90-120$</p>
+          </div>
+          <div>
+            <p>Posted Dec. 1. 2022</p>
+          </div>
+          <div>
+            <nuxt-link
+              to="/browse/1"
+              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+              >Detail</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+      <div class="mt-4">
+        <div class="p-6 flex items-center justify-between bg-gray-100">
+          <div>
+            <h1 class="text-xl font-semibold">The job position</h1>
+            <p class="text-gray-600">The company name</p>
+          </div>
+          <div>
+            <h1 class="font-semibold">Worldwide</h1>
+            <p class="text-gray-600">$90-120$</p>
+          </div>
+          <div>
+            <p>Posted Dec. 1. 2022</p>
+          </div>
+          <div>
+            <nuxt-link
+              to="/browse/1"
+              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+              >Detail</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ dream_job_front/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="py-20 px6 bg-teal-700 text-center">
+      <h1 class="mb-12 text-5xl text-white">Find a job anywhere</h1>
+
+      <nuxt-link
+        to="/browse"
+        class="inline-block py-4 px-8 bg-teal-900 text-white rounded-xl"
+        >Browse</nuxt-link
+      >
+    </div>
+
+    <div class="py-10 px-6">
+      <h2 class="mb-8 text-2xl text-center">Newest jobs</h2>
+
+      <div class="space-y-4">
+        <div class="p-6 flex items-center justify-between bg-gray-100">
+          <div>
+            <h1 class="text-xl font-semibold">The job position</h1>
+            <p class="text-gray-600">The company name</p>
+          </div>
+          <div>
+            <h1 class="font-semibold">Worldwide</h1>
+            <p class="text-gray-600">$90-120$</p>
+          </div>
+          <div>
+            <p>Posted Dec. 1. 2022</p>
+          </div>
+          <div>
+            <nuxt-link
+              to="/browse/1"
+              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
+              >Detail</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
