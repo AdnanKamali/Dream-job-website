28567c80c5e8f31b577115a216084f27b0ccb74c
Adnan Kamali <adnankamali1246@gmail.com>
2023-04-10 17:45:55 +0330

    create job with component

--- dream_job_front/pages/browse/index.vue
+++ dream_job_front/pages/browse/index.vue
@@ -35,93 +35,14 @@
       </div>
     </div>
     <div class="col-span-3">
-      <div>
-        <div class="p-6 flex items-center justify-between bg-gray-100">
-          <div>
-            <h1 class="text-xl font-semibold">The job position</h1>
-            <p class="text-gray-600">The company name</p>
-          </div>
-          <div>
-            <h1 class="font-semibold">Worldwide</h1>
-            <p class="text-gray-600">$90-120$</p>
-          </div>
-          <div>
-            <p>Posted Dec. 1. 2022</p>
-          </div>
-          <div>
-            <nuxt-link
-              to="/browse/1"
-              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-              >Detail</nuxt-link
-            >
-          </div>
-        </div>
-      </div>
-      <div class="mt-4">
-        <div class="p-6 flex items-center justify-between bg-gray-100">
-          <div>
-            <h1 class="text-xl font-semibold">The job position</h1>
-            <p class="text-gray-600">The company name</p>
-          </div>
-          <div>
-            <h1 class="font-semibold">Worldwide</h1>
-            <p class="text-gray-600">$90-120$</p>
-          </div>
-          <div>
-            <p>Posted Dec. 1. 2022</p>
-          </div>
-          <div>
-            <nuxt-link
-              to="/browse/1"
-              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-              >Detail</nuxt-link
-            >
-          </div>
-        </div>
-      </div>
-      <div class="mt-4">
-        <div class="p-6 flex items-center justify-between bg-gray-100">
-          <div>
-            <h1 class="text-xl font-semibold">The job position</h1>
-            <p class="text-gray-600">The company name</p>
-          </div>
-          <div>
-            <h1 class="font-semibold">Worldwide</h1>
-            <p class="text-gray-600">$90-120$</p>
-          </div>
-          <div>
-            <p>Posted Dec. 1. 2022</p>
-          </div>
-          <div>
-            <nuxt-link
-              to="/browse/1"
-              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-              >Detail</nuxt-link
-            >
-          </div>
-        </div>
-      </div>
-      <div class="mt-4">
-        <div class="p-6 flex items-center justify-between bg-gray-100">
-          <div>
-            <h1 class="text-xl font-semibold">The job position</h1>
-            <p class="text-gray-600">The company name</p>
-          </div>
-          <div>
-            <h1 class="font-semibold">Worldwide</h1>
-            <p class="text-gray-600">$90-120$</p>
-          </div>
-          <div>
-            <p>Posted Dec. 1. 2022</p>
-          </div>
-          <div>
-            <nuxt-link
-              to="/browse/1"
-              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-              >Detail</nuxt-link
-            >
-          </div>
-        </div>
+      <div class="space-y-4">
+        <Job />
+
+        <Job />
+
+        <Job />
+
+        <Job />
       </div>
     </div>
   </div>
--- dream_job_front/pages/index.vue
+++ dream_job_front/pages/index.vue
@@ -14,26 +14,11 @@
       <h2 class="mb-8 text-2xl text-center">Newest jobs</h2>
 
       <div class="space-y-4">
-        <div class="p-6 flex items-center justify-between bg-gray-100">
-          <div>
-            <h1 class="text-xl font-semibold">The job position</h1>
-            <p class="text-gray-600">The company name</p>
-          </div>
-          <div>
-            <h1 class="font-semibold">Worldwide</h1>
-            <p class="text-gray-600">$90-120$</p>
-          </div>
-          <div>
-            <p>Posted Dec. 1. 2022</p>
-          </div>
-          <div>
-            <nuxt-link
-              to="/browse/1"
-              class="py-4 px-6 bg-teal-700 text-white rounded-xl"
-              >Detail</nuxt-link
-            >
-          </div>
-        </div>
+        <Job />
+
+        <Job />
+
+        <Job />
       </div>
     </div>
   </div>
